1658855db76e20352ffc320c5428b41d5d49a852
Rosana Damasceno <rosanaxica7@yahoo.com.br>
2020-07-27 20:43:47 -0300

    primeiros passos em javascript - variaveis

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var numeroInteiroPositivo = 10;
+var numeroInteiroNegativo = -5;
+var numeroDecimal = 3;
+var string = "Sou uma string!";
+var booleano = true;
+
+console.log(numeroDecimal, numeroInteiroNegativo, numeroInteiroPositivo, string, booleano);
+
+var nome = "Rosana";
+var sobrenome = "Damasceno";
+
+console.log(nome + " " + sobrenome);
+
+console.log(nome, sobrenome);
+
+var texto = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis  repudiandae aspernatur excepturi non tempore debitis iusto rerum molestiae quibusdam, assumenda error harum ad cumque accusantium est facilis. Alias";
+
+var valorNulo = null;
+var naoDefinido;
+
+console.log(valorNulo, naoDefinido);
+
+var arrayDeFrutas = ["maça", "tomate", "abacate", "abacaxi", "laranja"]
+
+console.log(arrayDeFrutas)
+
+var digitalHouse = "Digital House, a escola de programação que forma novas gerações de coders e profissionais digitais para imaginar, inovar e criar o que sempre sonharam.";
